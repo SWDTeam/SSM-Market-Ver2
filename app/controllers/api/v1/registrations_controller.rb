1f24bd6886affe3996da8619393c2caffe6ccc48
Yen Minh <minhhy2897@gmail.com>
2018-06-27 20:29:13 +0700

    template

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,35 +1,38 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_params_exist
+  before_action :ensure_params_exist, only: [:create]
 
   respond_to :json
-  #{"account": {"email" :}}
+  # {"registration": {"email" : ""}}
   
   def create
     account = Account.new(account_params)
     if account.save
-      # render json: {message: "Registration has been completed", account: account}, status: 200
+      puts "THANH CONG ROI BE OI"      
       render json: account.as_json.merge({email: account.email, name: account.name, success: true}).to_json, status: 200
-      return
-    else
-      warden.custom_failure!
-      render json: account.errors, status: 200
+      return 
     end
+    warden.custom_failure!
+    render json: account.errors, status: 200
   end
 
 
   protected
-
   def ensure_params_exist
-    if params[:account][:email].blank? || params[:account][:password].blank? || 
-      params[:account][:password_confirmation].blank? || params[:account][:name].blank? ||
-      params[:account][:role_id].blank?
+    if params[:registration][:email].blank? || params[:registration][:password].blank? || 
+      params[:registration][:name].blank? 
+      # || params[:registration][:role_id].blank? || 
+      # params[:registration][:gender].blank? || 
+      # params[:registration][:address].blank? || params[:registration][:phone].blank?
       render json: {success: false, message: "missing sign up parameter"}, status: 422 
     end
   end
-  
+
   
   private
   def account_params
-    params.require(:account).permit(:email, :password, :password_confirmation, :name, :role_id)
+    params.require(:registration).permit(:email, :password, :name)
   end
+
+
+  # , :gender, :address, :phone, :role_id
 end
