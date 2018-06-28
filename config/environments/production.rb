95846e81416bc5cc5cc53e245d3e85c8d904324d
Yen Minh <minhhy2897@gmail.com>
2018-06-28 09:37:30 +0700

    fix production mail heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'ssm-market.herokuapp.com', port: 3000 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     user_name:      'ssm.market.222@gmail.com',
     password:       'ssmmarket11',
-    domain:         'localhost:3000',
+    domain:         'ssm-market.herokuapp.com',
     address:        'smtp.gmail.com',
     port:           '587',
     authentication: :plain,
