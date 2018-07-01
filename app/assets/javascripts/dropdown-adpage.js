463008a769010447f9a4461126afd6776fe7f9dd
Thien Phuc <ttran.phuc99@gmail.com>
2018-07-01 14:36:39 +0700

    update menu

--- app/assets/javascripts/dropdown-adpage.js
+++ app/assets/javascripts/dropdown-adpage.js
@@ -10,3 +10,24 @@ function dropDownAdPage() {
     ele[0].classList.add("fa-angle-down");
   }
 }
+
+function dropContentMenu(id, index) {
+  var field = document.getElementById(id);
+  field.classList.toggle("show-adpage");
+
+  /*if (field.classList.contains("show-adpage")) {
+    field.style.height = "auto";
+  } else {
+    field.style.height = "0";
+  }*/
+
+  var ele = document.getElementsByClassName("change-icon")[index];
+
+  if (ele.classList.contains("fa-angle-down")) {
+    ele.classList.remove("fa-angle-down");
+    ele.classList.add("fa-angle-up");
+  } else {
+    ele.classList.remove("fa-angle-up");
+    ele.classList.add("fa-angle-down");
+  }
+}
