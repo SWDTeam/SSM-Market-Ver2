87726302f9740254c71b68e9968f48402171d40f
Yen Minh <minhhy2897@gmail.com>
2018-07-17 17:08:17 +0700

    test

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     account = Account.new(account_params)
     puts "Params cua acccount " + account_params.to_s
+    account.skip_confirmation!
     if account.save
-      account.skip_confirmation!
       puts "THANH CONG ROI BE OI"      
       render json: account.as_json.merge({email: account.email, name: account.name, success: true}).to_json, status: 200
       return 
