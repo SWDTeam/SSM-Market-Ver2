a939ce14d6633cd58975c579d91f4e172a47288a
Thien Phuc <ttran.phuc99@gmail.com>
2018-07-19 12:13:48 +0700

    snackbar responsive

--- app/assets/javascripts/P-snackbar.js
+++ app/assets/javascripts/P-snackbar.js
@@ -7,14 +7,20 @@ function snackbarShow() {
   if (x !== null) {
     x.className = "show";
     var width = $("#notice").css("width");
+    var screenWidth = $(window).width();
 
     width = parseInt(width);
     width = width/2;
 
-    var flag = $("#sidebar").css("marginLeft");
+    if (screenWidth <= 400) {
+      $("#notice").css("width", screenWidth+"px");
+      $("#notice").css("marginLeft", "-" +screenWidth/2+ "px");
+    } else {
+      var flag = $("#sidebar").css("marginLeft");
 
-    if (flag === undefined || flag === "-250px") {
-      $("#notice").css("marginLeft", "-" +width+ "px");
+      if (flag === undefined || flag === "-250px") {
+        $("#notice").css("marginLeft", "-" +width+ "px");
+      }
     }
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
